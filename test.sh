29ff2fdcb6f47001e3f51b68ce726db348825673
Jeffrey Lau <who.else.at.jlau.tk>
2020-04-21 20:36:40 +0800

    Make sure tests return 0

--- test.sh
+++ test.sh
@@ -1,8 +1,37 @@
-#! /bin/sh
+#! /bin/bash
 
-for name in token tokenizer ah3 ah4 type;
-do
-    echo "Running test test-$name.py"
-    python test-$name.py
-done
-grep '[^a-z]set(' *.py |grep -v utils.py
+declare -a results=()
+
+run_tests() {
+	local rv
+	for name in token tokenizer ah3 ah4 type;
+	do
+		echo "Running test test-$name.py"
+		python test-$name.py
+		rv=$?
+		results+=(
+		"$name":::"$rv"
+		)
+	done
+}
+
+print_summary() {
+	for name__rv in "${results[@]}"
+	do
+		local name="${name__rv%:::*}"
+		local rv="${name__rv#*:::}"
+		echo "For test-${name}, rv is ${rv}"
+	done
+}
+
+greppy() {
+	grep '[^a-z]set(' -- *.py | grep -v utils.py
+}
+
+main() {
+	run_tests
+	print_summary
+	# greppy
+}
+
+main "$@"
